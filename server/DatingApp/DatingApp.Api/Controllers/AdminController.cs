308692850c03cd8c97b4e143d0d1ac45b8d6537a
relog <relogotech@gmail.com>
2025-04-13 19:49:27 +0700

    hobby req and admin controller with handler added

---
+++ server/DatingApp/DatingApp.Api/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using DatingApp.Application.Interfaces;
+using DatingApp.Application.Models.Requests;
+using DatingApp.Application.Services;
+using DatingApp.Domain.Entities;
+using DatingApp.Domain.Interfaces.Repositories;
+using DatingApp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Api.Controllers
+{
+	[ApiController]
+	[Route("api/v1/[controller]")]
+	public class AdminController : ControllerBase
+	{
+		private readonly ILogger<AuthController> _logger;
+		private readonly IHobbyRepository _hobbyRepository;
+
+		public AdminController(ILogger<AuthController> logger, IHobbyRepository hobbyRepository)
+		{
+			_logger = logger;
+			_hobbyRepository = hobbyRepository;
+		}
+
+		[Authorize(Policy = "admin")]
+		[HttpPost("hobbies")]
+		public async Task<ActionResult> AddHobbies
+			(HobbyRequest addedHobby, CancellationToken cancellationToken)
+		{
+			var newHobby = new Hobby
+			{
+				Id = Guid.NewGuid(),
+				Name = addedHobby.Name,
+				Description = addedHobby.Description,
+			};
+
+			var result = await _hobbyRepository.AddAsync(newHobby, cancellationToken);
+
+			if (result.IsError)
+			{
+				return BadRequest();
+			}
+
+			return Ok(result.Value);
+		}
+	}
+}
---
+++ server/DatingApp/DatingApp.Application/Models/Requests/HobbyRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Application.Models.Requests
+{
+	public class HobbyRequest
+	{
+		[Required]
+		[MaxLength(50)]
+		public string Name { get; set; }
+
+		[Required]
+		[MaxLength(150)]
+		public string Description { get; set; }
+	}
+}
