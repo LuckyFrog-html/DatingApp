fb47891fd9f730397cafc3c14120a755f0541785
Ivan <luckycoderdev@mail.ru>
2025-04-14 11:38:48 +0700

    user card

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { UserSwiper } from "@/widgets/ui";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
+
+import "./page.css";
 
 const Search = () => {
     const [ratio, setRatio] = useState(0);
+    const [isTransition, setIsTransition] = useState(false);
+
+    const [clickKludge, setClickKludge] = useState(0);
 
     const getSizeFromRatio = (ratio: number, positive: boolean) => {
         let val;
@@ -18,31 +23,61 @@ const Search = () => {
         return Math.max(val, 40);
     };
 
-    console.log(getSizeFromRatio(ratio, true));
+    console.log(ratio);
+
+    const onSwipeEnded = () => {
+        console.log("onSwipeEnded");
+        // setRatio(0);
+        // setIsTransition(true);
+    };
+
+    const onSwipeSuccessEnded = () => {
+        // setRatio(0);
+
+        console.log("onSwipeSuccessEnded");
+    };
+
+    const onDislikeClick = () => {
+        setClickKludge((prev) => prev + 1);
+
+        setRatio(-1);
+    };
+
+    const onLikeClick = () => {
+        setClickKludge((prev) => prev + 1);
+        setRatio(1);
+    };
 
     return (
         <div className="relative">
             <div
                 className={twMerge(
-                    "absolute left-0 top-[calc(50%-50px)] rounded-full w-[100px] h-[100px] bg-blue-600 text-white flex items-center justify-center",
+                    "z-20 absolute left-0 top-[calc(50%-50px)] rounded-full w-[150px] h-[150px] bg-blue-600 text-white flex items-center justify-center arrow",
                 )}
+                onClick={onDislikeClick}
                 style={{
                     transform: `scale(${getSizeFromRatio(ratio, true)}%)`,
                 }}>
-                <p>Не нрав</p>
+                <p className="text-3xl">Не нрав</p>
             </div>
             <UserSwiper
+                clickKludge={clickKludge}
+                like={onLikeClick}
+                dislike={onDislikeClick}
                 leftRightRatio={ratio}
                 setLeftRightRatio={setRatio}
+                onSwipeEnded={onSwipeEnded}
+                onSwipeSuccessEnded={onSwipeSuccessEnded}
                 user={"sad"}></UserSwiper>
             <div
                 className={twMerge(
-                    "absolute right-0 top-[calc(50%-50px)] rounded-full w-[100px] h-[100px] bg-red-400 text-white flex items-center justify-center",
+                    "z-20 absolute right-0 top-[calc(50%-50px)] rounded-full w-[150px] h-[150px] bg-red-400 text-white flex items-center justify-center arrow",
                 )}
+                onClick={onLikeClick}
                 style={{
                     transform: `scale(${getSizeFromRatio(ratio, false)}%)`,
                 }}>
-                <p>Нрав!</p>
+                <p className="text-3xl">Нрав!</p>
             </div>
         </div>
     );
--- client/src/widgets/ui/UserSwiper.tsx
+++ client/src/widgets/ui/UserSwiper.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { animateValue } from "@/app/shared/lib/hooks/animate";
 import { useWindowCenter } from "@/app/shared/lib/hooks/useWindowScreen";
 import { useEffect, useRef, useState } from "react";
 import { SwipeEventData, useSwipeable } from "react-swipeable";
@@ -7,28 +8,42 @@ import { SwipeEventData, useSwipeable } from "react-swipeable";
 type Props = {
     user: any;
     onSwipeEnded?: () => void;
+    onSwipeSuccessEnded?: () => void;
     leftRightRatio: number;
     setLeftRightRatio: (val: number) => void;
+    like: () => void;
+    dislike: () => void;
+    clickKludge: number;
 };
 
-const X_OFFSET_RATIO = 0.1;
+const X_OFFSET_RATIO = 0.2;
 const ROTATION_POINT_RATIO = 0.01;
 const CARD_WIDTH = 300;
 const CARD_HEIGHT = 500;
 const CONTAINER_WIDTH = 1280;
+const CARD_ON_SUCCESS_SPEED = 0.3;
+const CARD_ON_CLICK_SPEED = 3;
 
 export const UserSwiper = ({
     user,
     onSwipeEnded,
+    onSwipeSuccessEnded,
     leftRightRatio,
     setLeftRightRatio,
+    dislike,
+    like,
+    clickKludge,
 }: Props) => {
     const center = useWindowCenter();
+
+    const innerRef = useRef<HTMLDivElement>(null);
+    const outerRef = useRef<HTMLDivElement>(null);
 
     const [rotationPoint, setRotationPoint] = useState([0, 0]);
     const [rotation, setRotation] = useState(0);
     const [resXOffset, setResXOffset] = useState(0);
     const [transition, setTransition] = useState(false);
+    const [cursorAtLeft, setCursorAtLeft] = useState(false);
     const [yCompensation, setYCompensation] = useState(0);
 
     const [swipeStarted, setSwipeStarted] = useState(false);
@@ -52,14 +67,44 @@ export const UserSwiper = ({
         requestAnimationFrame(update);
     }
 
+    const leftRightRatioRef = useRef(leftRightRatio);
+
+    // Update the ref on each render when leftRightRatio changes:
     useEffect(() => {
-        // setYCompensation(-center[1]);
+        leftRightRatioRef.current = leftRightRatio;
+    }, [leftRightRatio]);
+
+    // useEffect to trigger internal logic when leftRightRatio is set externally:
+    useEffect(() => {
+        console.log("хуле ты прокаешь еблан");
+        if (
+            leftRightRatioRef.current === 1 ||
+            leftRightRatioRef.current === -1
+        ) {
+            // logic to run for "like"
+            console.log(
+                "UserSwiper: Like triggered via external button",
+                clickKludge,
+            );
+            // For instance, run an animation or update internal state:
+            // (You can even call successHandler() or any function)
+            successHandler(true);
+            // Optionally, reset state if needed:
+            // setLeftRightRatio(0);
+        } else if (leftRightRatio === -1) {
+            // logic to run for "dislike"
+            console.log("UserSwiper: Dislike triggered via external button");
+            successHandler(true);
+            // setLeftRightRatio(0);
+        }
+    }, [clickKludge]); // dependency array
+
+    useEffect(() => {
+        swipeEndedHandler();
         setRotationPoint([center[0], center[1]]);
     }, [center]);
 
     const swipingHandler = (eventData: SwipeEventData) => {
-        // setSwipeStarted(true);
-
         const e = eventData;
         const deltaMidX = eventData.initial[0] + eventData.deltaX - center[0];
         const deltaMidY = eventData.initial[1] + eventData.deltaY - center[1];
@@ -102,16 +147,33 @@ export const UserSwiper = ({
         setResXOffset(deltaMidX * X_OFFSET_RATIO);
         // console.log(deltaMidX, CONTAINER_WIDTH / 2, deltaMidX / (CONTAINER_WIDTH / 2), Math.min(deltaMidX / (CONTAINER_WIDTH / 2), 1), Math.max(Math.min(deltaMidX / (CONTAINER_WIDTH / 2), 1), -1));
         // if ()
-        setLeftRightRatio(Math.max(Math.min(deltaMidX / (CONTAINER_WIDTH / 2), 1), -1));
+        setLeftRightRatio(
+            Math.max(Math.min(deltaMidX / (CONTAINER_WIDTH / 2), 1), -1),
+        );
     };
 
     const swipeStartedHandler = (eventData: SwipeEventData) => {
         // setSwipeStarted(true);
         // setTimeout(() => setSwipeStarted(false), 300);
         setTransition(false);
+        // giveTempTransition();
     };
 
-    const swipeEndedHandler = (eventData: SwipeEventData) => {
+    const swipeEndedHandler = () => {
+        if (leftRightRatio === 1 || leftRightRatio === -1) {
+            onSwipeSuccessEnded?.();
+            successHandler();
+
+            // if (leftRightRatio === 1) {
+            //     like();
+            // } else {
+            //     dislike();
+            // }
+
+            return;
+        }
+
+        onSwipeEnded?.();
         setTransition(true);
         setResXOffset(0);
         setRotation(0);
@@ -126,11 +188,83 @@ export const UserSwiper = ({
         trackMouse: true,
     });
 
+    const successHandler = (isClick = false) => {
+        console.log("helo is click", isClick);
+        // setTransition(true);
+        setRotation(0);
+        animateValue({
+            from: 0,
+            to: 1,
+            duration: 0.6,
+            // duration: isClick ? 0.6 : 0.4,
+            onUpdate: (val) => {
+                // console.log(
+                //     "!isClick",
+                //     !isClick,
+                //     !isClick ? CARD_ON_SUCCESS_SPEED : CARD_ON_CLICK_SPEED,
+                //     ((val * window.innerWidth) / 2) *
+                //         (!isClick
+                //             ? CARD_ON_SUCCESS_SPEED
+                //             : CARD_ON_CLICK_SPEED),
+                //     leftRightRatio > 0
+                //         ? resXOffset +
+                //               ((val * window.innerWidth) / 2) *
+                //                   (!isClick
+                //                       ? CARD_ON_SUCCESS_SPEED
+                //                       : CARD_ON_CLICK_SPEED)
+                //         : resXOffset -
+                //               ((val * window.innerWidth) / 2) *
+                //                   (!isClick
+                //                       ? CARD_ON_SUCCESS_SPEED
+                //                       : CARD_ON_CLICK_SPEED),
+                // );
+                setResXOffset((prev) =>
+                    leftRightRatio > 0
+                        ? prev +
+                          ((val * window.innerWidth) / 2) *
+                              (isClick
+                                  ? CARD_ON_SUCCESS_SPEED
+                                  : CARD_ON_CLICK_SPEED)
+                        : prev -
+                          ((val * window.innerWidth) / 2) *
+                              (isClick
+                                  ? CARD_ON_SUCCESS_SPEED
+                                  : CARD_ON_CLICK_SPEED),
+                );
+            },
+            onComplete: () => {
+                console.log("onComplete");
+                setResXOffset(0);
+            },
+        });
+        animateValue({
+            from: 1,
+            to: 0,
+            // duration: isClick ? 0.2 : 0.3,
+            duration: 0.2,
+            onUpdate: (val) => {
+                if (!outerRef.current) return;
+                outerRef.current.style.opacity = val.toString();
+            },
+        });
+        setTimeout(
+            () => {
+                if (!outerRef.current) return;
+                console.log("setTimeout");
+                outerRef.current.style.opacity = "1";
+                setTransition(true);
+            },
+            // isClick ? 800 : 500,
+            800,
+        );
+    };
+
     return (
         <div
             {...handlers}
-            className="z-40 relative h-[calc(100vh-60px)] flex flex-col items-center justify-center">
+            className="z-10 relative h-[calc(100vh-60px)] flex flex-col items-center justify-center">
             <div
+                ref={outerRef}
                 style={{
                     transform: `translateX(${resXOffset}px)`,
                     transition: transition ? "all 0.3s ease" : "none",
@@ -139,12 +273,15 @@ export const UserSwiper = ({
             >
                 {/* Inner container: applies rotation with custom transform origin */}
                 <div
+                    ref={innerRef}
                     style={{
                         transform: `rotate(${rotation}rad)`,
-                        transformOrigin: `${rotationPoint[0]}px ${rotationPoint[1]}px`,
+                        // transformOrigin: `${rotationPoint[0]}px ${rotationPoint[1]}px`,
                         transition: transition ? "all 0.3s ease" : "none",
+                        width: `${CARD_WIDTH}px`,
+                        height: `${CARD_HEIGHT}px`,
                     }}
-                    className={`h-[${CARD_HEIGHT}px] w-[${CARD_WIDTH}px] rounded-2xl bg-blue-500`}
+                    className={`rounded-2xl bg-blue-500`}
                 />
             </div>
             {/* <div
